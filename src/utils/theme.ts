d55b6045717a596c21879f6a7bc34708fe897ba1
Victor Lebecq <victor.lebecq.pro@gmail.com>
2025-03-20 14:08:07 +0100

    (refactor) : made a utils folder to store reusable functions

---
+++ src/utils/theme.ts
@@ -0,0 +1,5 @@
+export function toggleTheme() {
+  const currentTheme = document.body.getAttribute('data-theme');
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  document.body.setAttribute('data-theme', newTheme);
+}
