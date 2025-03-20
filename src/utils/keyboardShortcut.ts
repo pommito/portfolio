cb8627b08ab01641ce7ca311cf0c3e36694fb528
Victor Lebecq <victor.lebecq.pro@gmail.com>
2025-03-20 14:44:22 +0100

    (feat): add keyboard shortcut support for header nav

---
+++ src/utils/keyboardShortcut.ts
@@ -0,0 +1,10 @@
+export function handleKeyboardShortcut(e: KeyboardEvent) {
+  switch (e.key) {
+    case 'i':
+      window.location.href = '/about';
+      break;
+    case 'b':
+      window.location.href = '/blog';
+      break;
+  }
+}
