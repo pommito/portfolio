fca08735086988aef6fafaebc87ae412a4f8b392
Victor Lebecq <victor.lebecq.pro@gmail.com>
2025-03-28 23:15:18 +0100

    (refactor) : separate launcher code to make it more clear and scalable

---
+++ src/data/launcher.ts
@@ -0,0 +1,14 @@
+export const categories = [
+    {
+        label: 'Pages',
+        entries: ['Accueil', 'A propos', 'Blog'],
+    },
+    {
+        label: 'Réglages',
+        entries: ['Changer de thème', 'Changer de langue'],
+    },
+    {
+        label: 'Contact',
+        entries: ['Envoyer un email', 'Github'],
+    },
+]
