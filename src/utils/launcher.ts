de43df0723e3198e8d4962061332555e32be24b7
Victor Lebecq <victor.lebecq.pro@gmail.com>
2025-03-29 13:10:30 +0100

    (refactor) : remove all the debuging logs

--- src/utils/launcher.ts
+++ src/utils/launcher.ts
@@ -1,30 +1,22 @@
 export const toggleModal = () => {
-    console.log('toggleModal trigger')
-
     const LauncherElem = document.getElementById('search-menu')
 
     if (!LauncherElem) return
     const isModalOpen = LauncherElem.getAttribute('data-state') === 'open'
 
     if (isModalOpen) {
-        console.log('Closing launcher.', isModalOpen)
-
         document.body.setAttribute('data-modal', 'close')
         LauncherElem.setAttribute('data-state', 'close')
     } else {
-        console.log('Opening launcher.', isModalOpen)
         document.body.setAttribute('data-modal', 'open')
         LauncherElem.setAttribute('data-state', 'open')
     }
 }
 
 export const handleEscShortcut = (e: KeyboardEvent) => {
-    console.log('handleEscShortcut')
-
     if (e.key !== 'Escape') return
     const searchMenuElem = document.getElementById('search-menu')
     if (searchMenuElem?.getAttribute('data-state') === 'open') {
-        console.log('handleEscShortcut triggered')
         toggleModal()
     }
 }
