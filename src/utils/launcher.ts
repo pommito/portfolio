b92078f3d5948c98ece450ee29bcf66fd3e88437
Victor Lebecq <victor.lebecq.pro@gmail.com>
2025-03-29 10:25:54 +0100

    (feat) : user can now press enter and navigate thourgh the page with Launcher

--- src/utils/launcher.ts
+++ src/utils/launcher.ts
@@ -1,16 +1,13 @@
 export const toggleModal = (modalElem: HTMLElement) => {
     const isModalOpen = modalElem.getAttribute('data-state') === 'open'
-    // const inputElem = modalElem.querySelector('input') as HTMLInputElement
 
     if (isModalOpen) {
         modalElem.setAttribute('data-state', 'close')
         document.body.setAttribute('data-modal', 'close')
-        // inputElem.value = ''
         window.removeEventListener('keydown', handleEscShortcut)
     } else {
         modalElem.setAttribute('data-state', 'open')
         document.body.setAttribute('data-modal', 'open')
-        // inputElem.focus()
         window.addEventListener('keydown', handleEscShortcut)
     }
 }
