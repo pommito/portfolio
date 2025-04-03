9c49c1232f5d8ab939b5b8edc4a16876b0eb5219
Victor Lebecq <victor.lebecq.pro@gmail.com>
2025-04-03 15:00:46 +0200

    (feat/theme) : implement theme persistence and local storage saving

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,5 +1,29 @@
 export function toggleTheme() {
-  const currentTheme = document.body.getAttribute('data-theme');
-  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-  document.body.setAttribute('data-theme', newTheme);
+    const currentTheme = document.body.getAttribute('data-theme')
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark'
+    document.body.setAttribute('data-theme', newTheme)
+}
+
+export const checkSavedTheme = () => {
+    if (!localStorage) return
+    const savedTheme = localStorage.getItem('theme')
+    if (!savedTheme) {
+        console.log('No theme saved')
+        return
+    }
+
+    document.body.setAttribute('data-theme', savedTheme)
+    console.log(`theme saved : ${savedTheme}`)
+}
+
+export const saveThemeChoice = () => {
+    if (!localStorage) return
+    const currentTheme = document.body.getAttribute('data-theme')
+    if (!currentTheme) {
+        console.log('Error: no theme on the body')
+        return
+    }
+
+    localStorage.setItem('theme', currentTheme)
+    console.log(`theme saved : ${currentTheme}`)
 }
