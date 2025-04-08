97bd77b1c8466f6e80f6d80541e4c2e414f145b2
Victor Lebecq <victor.lebecq.pro@gmail.com>
2025-04-08 10:11:20 +0200

    (fix) : LanguagePicker is now redirecting to the translate version of the page if it exist

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -27,3 +27,35 @@ export function useTranslatedPath(lang: keyof typeof ui) {
             : `/${l}${translatedPath}`
     }
 }
+
+export function getRouteFromUrl(url: URL): string | undefined {
+    const pathname = new URL(url).pathname
+    const parts = pathname?.split('/')
+    const path = parts.pop() || parts.pop()
+
+    if (path === undefined) {
+        return undefined
+    }
+
+    const currentLang = getLangFromUrl(url)
+
+    if (defaultLang === currentLang) {
+        const route = Object.values(routes)[0]
+        return route[path] !== undefined ? route[path] : undefined
+    }
+
+    const getKeyByValue = (
+        obj: Record<string, string>,
+        value: string
+    ): string | undefined => {
+        return Object.keys(obj).find((key) => obj[key] === value)
+    }
+
+    const reversedKey = getKeyByValue(routes[currentLang], path)
+
+    if (reversedKey !== undefined) {
+        return reversedKey
+    }
+
+    return undefined
+}
