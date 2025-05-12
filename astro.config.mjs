65f696694f4348a43697f9c3b1063355db7fc5b8
Victor Lebecq <victor.lebecq.pro@gmail.com>
2025-05-12 11:29:17 +0200

    (feat) : configuration of the sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import sitemap from '@astrojs/sitemap'
+
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://victorlebecq.fr',
     experimental: {
         svg: true,
     },
+
     vite: {
         plugins: [tailwindcss()],
     },
+
     i18n: {
         locales: ['fr', 'en'],
         defaultLocale: 'fr',
@@ -18,4 +23,16 @@ export default defineConfig({
             prefixDefaultLocale: false,
         },
     },
+
+    integrations: [
+        sitemap({
+            i18n: {
+                locales: {
+                    fr: 'fr-FR',
+                    en: 'en-US',
+                },
+                defaultLocale: 'fr',
+            },
+        }),
+    ],
 })
