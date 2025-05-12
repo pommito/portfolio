8d292c5a2e4bc2ad78b3db24f9dd556b2b57345b
Victor Lebecq <victor.lebecq.pro@gmail.com>
2025-05-12 12:43:57 +0200

    (chore) : upgrade astro to the latest version

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,39 +5,39 @@ import tailwindcss from '@tailwindcss/vite'
 
 import sitemap from '@astrojs/sitemap'
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://victorlebecq.fr',
+    site: 'https://victorlebecq.fr',
 
-  experimental: {
-      svg: true,
-  },
+    // experimental: {
+    //     svg: true,
+    // },
 
-  vite: {
-      plugins: [tailwindcss()],
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 
-  i18n: {
-      locales: ['fr', 'en'],
-      defaultLocale: 'fr',
-      routing: {
-          prefixDefaultLocale: false,
-      },
-  },
+    i18n: {
+        locales: ['fr', 'en'],
+        defaultLocale: 'fr',
+        routing: {
+            prefixDefaultLocale: false,
+        },
+    },
 
-  integrations: [
-      sitemap({
-          i18n: {
-              locales: {
-                  fr: 'fr-FR',
-                  en: 'en-US',
-              },
-              defaultLocale: 'fr',
-          },
-      }),
-  ],
+    integrations: [
+        sitemap({
+            i18n: {
+                locales: {
+                    fr: 'fr-FR',
+                    en: 'en-US',
+                },
+                defaultLocale: 'fr',
+            },
+        }),
+    ],
 
-  adapter: cloudflare(),
+    adapter: cloudflare(),
 })
