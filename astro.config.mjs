2717596a5e1fe74bc0559a0687756a86691283ea
Victor Lebecq <victor.lebecq.pro@gmail.com>
2025-05-12 12:36:35 +0200

    (feat) : add the cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,34 +5,39 @@ import tailwindcss from '@tailwindcss/vite'
 
 import sitemap from '@astrojs/sitemap'
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://victorlebecq.fr',
-    experimental: {
-        svg: true,
-    },
+  site: 'https://victorlebecq.fr',
 
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  experimental: {
+      svg: true,
+  },
 
-    i18n: {
-        locales: ['fr', 'en'],
-        defaultLocale: 'fr',
-        routing: {
-            prefixDefaultLocale: false,
-        },
-    },
+  vite: {
+      plugins: [tailwindcss()],
+  },
 
-    integrations: [
-        sitemap({
-            i18n: {
-                locales: {
-                    fr: 'fr-FR',
-                    en: 'en-US',
-                },
-                defaultLocale: 'fr',
-            },
-        }),
-    ],
+  i18n: {
+      locales: ['fr', 'en'],
+      defaultLocale: 'fr',
+      routing: {
+          prefixDefaultLocale: false,
+      },
+  },
+
+  integrations: [
+      sitemap({
+          i18n: {
+              locales: {
+                  fr: 'fr-FR',
+                  en: 'en-US',
+              },
+              defaultLocale: 'fr',
+          },
+      }),
+  ],
+
+  adapter: cloudflare(),
 })
