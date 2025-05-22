f9723df32130cef233039dc6e0deab2effa9c404
Victor Lebecq <victor.lebecq.pro@gmail.com>
2025-05-22 16:29:22 +0200

    (refactor) : somes changes and optimisations

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,14 +1,14 @@
-export function toggleTheme() {
+export const toggleTheme = () => {
     const currentTheme = document.body.getAttribute('data-theme')
     const newTheme = currentTheme === 'dark' ? 'light' : 'dark'
     document.body.setAttribute('data-theme', newTheme)
+    saveThemeChoice()
 }
 
 export const checkSavedTheme = () => {
     if (!localStorage) return
     const savedTheme = localStorage.getItem('theme')
     if (!savedTheme) return
-
     document.body.setAttribute('data-theme', savedTheme)
 }
 
